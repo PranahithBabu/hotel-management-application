d59fd5eda13bf428265d8432392d30bc9d3af9f4
PranahithBabu <prana.yarra@gmail.com>
2024-03-23 19:06:37 -0400

    Files

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5000;
-
-export const mongoDBURL = 'mongodb+srv://root:root@hotel-management.q5mo2ur.mongodb.net/hotel-management?retryWrites=true&w=majority&appName=hotel-management';
