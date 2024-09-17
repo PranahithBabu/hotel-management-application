7c035de11b044fdb112980c2aef7440d14f10beb
PranahithBabu <59257365+PranahithBabu@users.noreply.github.com>
2024-09-17 19:23:15 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,14 +13,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
-app.use(cors({}));
+// app.use(cors({}));
 
-// app.use(cors({
-//     origin: '*',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true,
-//   }));
+app.use(cors({
+    origin: 'https://hotel-management-frontend-9fdq.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
 
 app.get('/', (req,res) => {
     res.status(200).send('In GET');
